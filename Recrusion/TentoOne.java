286db74693ab3ba08cc3ca2ae5b84dd4aae14106
Sarvesh <sarveshjaiofficial@gmail.com>
2024-06-30 20:32:27 +0530

    Updated New Program

--- Recrusion/TentoOne.java
+++ Recrusion/TentoOne.java
@@ -42,19 +42,86 @@ public class TentoOne {
     }
     
     public static int fibonacci(int n) {
-        
+
         if (n == 0 || n == 1) {
-            return n; 
+            return n;
         }
 
         return fibonacci(n - 1) + fibonacci(n - 2);
     }
+    
+    public static boolean sortedArray(int[] arr, int i) {
+
+        if (arr.length - 1 == i) {
+            return true;
+        }
+
+        if (arr[i] > arr[i + 1]) {
+            return false;
+        }
+
+        return sortedArray(arr, i + 1);
+
+    }
+    
+    public static int firstOccurrence(int[] arr, int key, int i) {
+
+        if (i == arr.length) {
+            return -1;
+        }
+
+        if (arr[i] == key) {
+            return i;
+        }
+
+        return firstOccurrence(arr, key, i + 1);
+    }
+    
+
+    public static int lastOccurrence(int[] arr, int key, int i) {
+
+        if (i == arr.length) {
+            return -1;
+        }
+
+        int isFound = lastOccurrence(arr, key, i + 1);
+
+        // if (isFound != -1) {
+        //     return isFound;
+        // }
+
+        // if (arr[i] == key) {
+        //     return i;
+        // }
+        if (isFound == -1 && arr[i] == key) {
+            return i;
+        }
+
+        return isFound;
+
+    }
+    
+    public static int power(int x, int n) {
+        
+        if (n == 0) {
+            return 1;
+        }
+
+        int fnm1 = power(x, n - 1);
+
+        return x * fnm1;
+    }
+    
+
 
     public static void main(String[] args) {
         int n = 10;
         printDec(n);
         printInc(n);
-
+        int[] arr = { 3,2,3,4,5 };
         System.err.println((fibonacci(5)));
+        System.err.println((sortedArray(arr,0)));
+        System.err.println((firstOccurrence(arr,3,0)));
+        System.err.println((lastOccurrence(arr,3,0)));
     }
 }
